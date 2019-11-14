9aac4efbee216c986b66a55700536cd2a462e656
Stephen Sung <stephensung@Waywards-MacBook-Pro.local>
2019-11-13 17:02:23 -0800

    first commit

---
+++ home.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Template name: Custom Home
+ * Template used for homepage.
+ *
+ * @package Avada
+ * 
+ */
+
+?>
+<?php get_header(); ?>
+<section id="content">
+	<p>Hello WORLD!</p>
+	
+</section>
+<?php do_action( 'avada_after_content' ); ?>
+<?php get_footer(); ?>
